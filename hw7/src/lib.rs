5b2819438247b6d6bb845d43b2eb71f702a3c201
Dmitry Pavlov <mitrickx@mail.ru>
2024-02-25 11:16:07 +0300

    hw7: procedure macro

---
+++ hw7/src/lib.rs
@@ -0,0 +1,32 @@
+fn foo() -> i32 {
+    1 + 2
+}
+
+fn bar() -> &'static str {
+    "Hello"
+}
+
+fn bas() -> [i32; 3] {
+    [1, 2, 3]
+}
+
+macro_rules! my_macro {
+    ($($i:ident),+ $(,)?) => {
+        ($(
+            $i(),
+        )+)
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn my_macro_works() {
+        let (foo_result, bar_result, bas_result) = my_macro!(foo, bar,bas);
+        assert_eq!(foo_result, foo());
+        assert_eq!(bar_result, bar());
+        assert_eq!(bas_result, bas());
+    }
+}
