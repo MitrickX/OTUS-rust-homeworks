9c4eed1259fd5af9c793b608f546dfa54dfe09e7
Dmitry Pavlov <mitrickx@mail.ru>
2024-02-25 20:05:19 +0300

    hw7: touch a little bit

--- hw7/src/lib.rs
+++ hw7/src/lib.rs
@@ -1,15 +1,4 @@
-fn foo() -> i32 {
-    1 + 2
-}
-
-fn bar() -> &'static str {
-    "Hello"
-}
-
-fn bas() -> [i32; 3] {
-    [1, 2, 3]
-}
-
+#[macro_export]
 macro_rules! my_macro {
     ($($i:ident),+ $(,)?) => {
         ($(
@@ -24,7 +13,19 @@ mod tests {
 
     #[test]
     fn my_macro_works() {
-        let (foo_result, bar_result, bas_result) = my_macro!(foo, bar,bas);
+        fn foo() -> i32 {
+            1 + 2
+        }
+
+        fn bar() -> &'static str {
+            "Hello"
+        }
+
+        fn bas() -> [i32; 3] {
+            [1, 2, 3]
+        }
+        
+        let (foo_result, bar_result, bas_result) = my_macro!(foo, bar, bas);
         assert_eq!(foo_result, foo());
         assert_eq!(bar_result, bar());
         assert_eq!(bas_result, bas());
