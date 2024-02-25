d99f282293b5152aec146736dbb7bd42ec397421
Dmitry Pavlov <mitrickx@mail.ru>
2024-02-25 20:08:32 +0300

    hw7 part2: function-like proc macro

---
+++ hw7_part2/src/lib.rs
@@ -0,0 +1,24 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::parse::Parser;
+
+#[proc_macro]
+pub fn my_macro(input: TokenStream) -> TokenStream {
+    let data = syn::punctuated::Punctuated::<syn::LitStr, syn::Token![,]>::parse_terminated
+        .parse(input)
+        .expect("support only string literals");
+
+    let elem: Vec<Ident> = data
+        .into_iter()
+        .map(|l| Ident::new(l.value().as_str(), Span::call_site()))
+        .collect();
+
+    let q = quote! {
+        (#( #elem(), )*)
+    };
+
+    q.into()
+}
