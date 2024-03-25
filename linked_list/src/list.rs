3cc39f1a00366f91da9b519938add6b6b239f3a0
Dmitry Pavlov <mitrickx@mail.ru>
2024-03-25 09:04:42 +0300

    linded list: move to mod, add 2 methods - push_after, to_vec

---
+++ linked_list/src/list.rs
@@ -0,0 +1,166 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub type NodeRef<T> = Rc<RefCell<Option<Node<T>>>>;
+
+pub struct Node<T> {
+    value: T,
+    next: NodeRef<T>,
+}
+
+impl<T> Node<T> {
+    fn new_none(value: T) -> Self {
+        Node { value, next: Rc::new(RefCell::new(None)) }
+    }
+
+    fn new_some(value: T, next: NodeRef<T>) -> Self {
+        Node { value, next }
+    }
+
+    fn new_none_ref(value: T) -> NodeRef<T> {
+        Rc::new(RefCell::new(Some(Node::new_none(value))))
+    }
+
+    fn new_some_ref(value: T, next: NodeRef<T>) -> NodeRef<T> {
+        Rc::new(RefCell::new(Some(Node::new_some(value, next))))
+    }
+}
+
+pub struct List<T> {
+    head: NodeRef<T>,
+    tail: NodeRef<T>,
+    size: usize,
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        let head = Rc::new(RefCell::new(None));
+        let tail = head.clone();
+        List { head, tail, size: 0 }
+    }
+
+    pub fn push_back(&mut self, value: T) {
+        let new_node_ref = Node::new_none_ref(value);
+
+        if let Some(ref mut node) = *self.tail.borrow_mut() {
+            node.next = new_node_ref.clone();
+        }
+
+        self.tail = new_node_ref.clone();
+
+        if self.head.borrow().is_none() {
+            self.head = self.tail.clone();
+        }
+
+        self.size += 1;
+    }
+
+    pub fn push_front(&mut self, value: T) {
+        if self.size == 0 {
+            self.push_back(value);
+            return;
+        }
+
+        let new_node_ref = Node::new_some_ref(value, self.head.clone());
+
+        self.head = new_node_ref.clone();
+
+        self.size += 1;
+    }
+
+    pub fn push_after(&mut self, idx: usize, value: T) {
+        if self.size <= idx {
+            self.push_back(value);
+            return;
+        }
+
+        let mut p = self.head.clone();
+
+        for _ in 0..idx {
+            if let Some(ref node) = *p.clone().borrow() {
+                p = node.next.clone();
+            };
+        }
+
+        if let Some(ref mut node) = *p.borrow_mut() {
+            let new_node_ref = Node::new_some_ref(value, node.next.clone());
+            node.next = new_node_ref.clone();
+        };
+
+        self.size += 1;
+    }
+}
+
+impl <T : Copy> List <T> {
+    pub fn to_vec(&self) -> Vec<T> {
+        let mut result = Vec::<T>::new();
+
+        let mut p = self.head.clone();
+        while let Some(ref node) = *p.clone().borrow() {
+            result.push(node.value);
+            p = node.next.clone();
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::list::List;
+
+    #[test]
+    fn push_back_works() {
+        let mut result = List::<i32>::new();
+        result.push_back(1);
+        result.push_back(2);
+        result.push_back(3);
+
+        assert_eq!("[1, 2, 3]", format!("{:?}", result.to_vec()));
+    }
+
+    #[test]
+    fn push_front_works() {
+        let mut result = List::<i32>::new();
+        result.push_front(1);
+        result.push_front(2);
+        result.push_front(3);
+
+        assert_eq!("[3, 2, 1]", format!("{:?}", result.to_vec()));
+    }
+
+    #[test]
+    fn push_after_works() {
+        let mut result = List::<i32>::new();
+
+        result.push_after(0, -1);
+
+        result.push_back(1);
+        result.push_back(2);
+        result.push_back(3);
+        result.push_front(0);
+
+        assert_eq!("[0, -1, 1, 2, 3]", format!("{:?}", result.to_vec()));
+
+        result.push_after(0, 100);
+        result.push_after(0, 101);
+        result.push_after(3, 200);
+        result.push_after(3, 201);
+
+        result.push_after(6, 300);
+        result.push_after(6, 301);
+
+        result.push_after(9, 400);
+        result.push_after(9, 401);
+
+        result.push_after(1000, 500);
+
+        assert_eq!("[0, 101, 100, -1, 201, 200, 1, 301, 300, 2, 401, 400, 3, 500]", format!("{:?}", result.to_vec()));
+    }
+}
