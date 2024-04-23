5c381ae656e3a36153f9753968ee157fa64ec470
Dmitry Pavlov <mitrickx@mail.ru>
2024-04-23 10:42:09 +0300

    bank_srv_cl: some handler tests

--- bank_srv_cl/server/src/server/handler.rs
+++ bank_srv_cl/server/src/server/handler.rs
@@ -547,4 +547,260 @@ mod tests {
 
         assert_eq!(from_utf8(writer.as_slice()).unwrap(), expected);
     }
+
+    #[test]
+    fn handle_deposit_works() {
+        let mut reader = "register_account 100".as_bytes();
+
+        let mut writer = Vec::new();
+        let mut terminal = Vec::new();
+
+        let mut context = Context::default();
+        handle(&mut context, &mut reader, &mut writer, &mut terminal).unwrap();
+
+        let operations: Vec<&Operation> = context.banks[context.current_bank]
+            .get_all_operations()
+            .collect();
+
+        let account_id = if let OperationKind::Register { id, .. } = operations[0].kind {
+            id
+        } else {
+            AccountID::new()
+        };
+
+        let input = vec![
+            "deposit".to_owned(),
+            "deposit test 10".to_owned(),
+            format!("deposit {} test", account_id.to_string()),
+            format!("deposit {} 100", account_id.to_string()),
+        ]
+        .join("\n");
+
+        let mut reader = input.as_bytes();
+
+        let mut context = context.clone();
+        handle(&mut context, &mut reader, &mut writer, &mut terminal).unwrap();
+
+        let operations: Vec<&Operation> = context.banks[context.current_bank]
+            .get_all_operations()
+            .collect();
+
+        let expected = vec![
+            format!(
+                "Bank: 1\nOpID: {}\nStatus: ok\nResult: {}\n\n",
+                operations[0].id, account_id,
+            ),
+            format!(
+                "Command: deposit\nStatus: error\nType: parse\nError: {}\n\n",
+                ParseError::RequireArguments(vec!["account_id".to_owned(), "amount".to_owned()]),
+            ),
+            format!(
+                "Command: deposit test 10\nStatus: error\nType: parse\nError: {}\n\n",
+                ParseError::InvalidArgumentAccountID(
+                    "account_id".to_owned(),
+                    AccountID::parse_str("test").unwrap_err()
+                )
+            ),
+            format!(
+                "Command: deposit {} test\nStatus: error\nType: parse\nError: {}\n\n",
+                account_id.to_string(),
+                ParseError::InvalidArgumentUint(
+                    "amount".to_owned(),
+                    "test".parse::<u64>().unwrap_err(),
+                )
+            ),
+            format!("Bank: 1\nOpID: {}\nStatus: ok\n\n", operations[1].id),
+        ]
+        .join("");
+
+        assert_eq!(from_utf8(writer.as_slice()).unwrap(), expected);
+    }
+
+    #[test]
+    fn handle_withdraw_works() {
+        let mut reader = "register_account 100".as_bytes();
+
+        let mut writer = Vec::new();
+        let mut terminal = Vec::new();
+
+        let mut context = Context::default();
+        handle(&mut context, &mut reader, &mut writer, &mut terminal).unwrap();
+
+        let operations: Vec<&Operation> = context.banks[context.current_bank]
+            .get_all_operations()
+            .collect();
+
+        let account_id = if let OperationKind::Register { id, .. } = operations[0].kind {
+            id
+        } else {
+            AccountID::new()
+        };
+
+        let input = vec![
+            "withdraw".to_owned(),
+            "withdraw test 10".to_owned(),
+            format!("withdraw {} test", account_id.to_string()),
+            format!("withdraw {} 100", account_id.to_string()),
+            format!("withdraw {} 100", account_id.to_string()),
+        ]
+        .join("\n");
+
+        let mut reader = input.as_bytes();
+
+        let mut context = context.clone();
+        handle(&mut context, &mut reader, &mut writer, &mut terminal).unwrap();
+
+        let operations: Vec<&Operation> = context.banks[context.current_bank]
+            .get_all_operations()
+            .collect();
+
+        let expected = vec![
+            format!(
+                "Bank: 1\nOpID: {}\nStatus: ok\nResult: {}\n\n",
+                operations[0].id, account_id,
+            ),
+            format!(
+                "Command: withdraw\nStatus: error\nType: parse\nError: {}\n\n",
+                ParseError::RequireArguments(vec!["account_id".to_owned(), "amount".to_owned()]),
+            ),
+            format!(
+                "Command: withdraw test 10\nStatus: error\nType: parse\nError: {}\n\n",
+                ParseError::InvalidArgumentAccountID(
+                    "account_id".to_owned(),
+                    AccountID::parse_str("test").unwrap_err()
+                )
+            ),
+            format!(
+                "Command: withdraw {} test\nStatus: error\nType: parse\nError: {}\n\n",
+                account_id.to_string(),
+                ParseError::InvalidArgumentUint(
+                    "amount".to_owned(),
+                    "test".parse::<u64>().unwrap_err(),
+                )
+            ),
+            format!("Bank: 1\nOpID: {}\nStatus: ok\n\n", operations[1].id),
+            "Bank: 1\nStatus: error\nType: bank\nError: Insufficient funds\n\n".to_owned(),
+        ]
+        .join("");
+
+        assert_eq!(from_utf8(writer.as_slice()).unwrap(), expected);
+    }
+
+    #[test]
+    fn handle_transfer_works() {
+        let input = vec![
+            "register_account 100".to_owned(),
+            "register_account 50".to_owned(),
+        ]
+        .join("\n");
+
+        let mut reader = input.as_bytes();
+
+        let mut writer = Vec::new();
+        let mut terminal = Vec::new();
+
+        let mut context = Context::default();
+        handle(&mut context, &mut reader, &mut writer, &mut terminal).unwrap();
+
+        let operations: Vec<&Operation> = context.banks[context.current_bank]
+            .get_all_operations()
+            .collect();
+
+        let account1_id = if let OperationKind::Register { id, .. } = operations[0].kind {
+            id
+        } else {
+            AccountID::new()
+        };
+
+        let account2_id = if let OperationKind::Register { id, .. } = operations[1].kind {
+            id
+        } else {
+            AccountID::new()
+        };
+
+        let input = vec![
+            "transfer".to_owned(),
+            "transfer test1 test2 50".to_owned(),
+            format!("transfer {} test2 50", account1_id.to_string()),
+            format!("transfer test1 {} 50", account2_id.to_string()),
+            format!(
+                "transfer {} {} test",
+                account1_id.to_string(),
+                account2_id.to_string()
+            ),
+            format!(
+                "transfer {} {} 50",
+                account1_id.to_string(),
+                account2_id.to_string()
+            ),
+            format!(
+                "transfer {} {} 500",
+                account2_id.to_string(),
+                account1_id.to_string()
+            ),
+        ]
+        .join("\n");
+
+        let mut reader = input.as_bytes();
+
+        let mut context = context.clone();
+        handle(&mut context, &mut reader, &mut writer, &mut terminal).unwrap();
+
+        let operations: Vec<&Operation> = context.banks[context.current_bank]
+            .get_all_operations()
+            .collect();
+
+        let expected = vec![
+            format!(
+                "Bank: 1\nOpID: {}\nStatus: ok\nResult: {}\n\n",
+                operations[0].id, account1_id,
+            ),
+            format!(
+                "Bank: 1\nOpID: {}\nStatus: ok\nResult: {}\n\n",
+                operations[1].id, account2_id,
+            ),
+            format!(
+                "Command: transfer\nStatus: error\nType: parse\nError: {}\n\n",
+                ParseError::RequireArguments(vec![
+                    "sender_account_id".to_owned(),
+                    "reciever_account_id".to_owned(),
+                    "amount".to_owned()
+                ]),
+            ),
+            format!(
+                "Command: transfer test1 test2 50\nStatus: error\nType: parse\nError: {}\n\n",
+                ParseError::InvalidArgumentAccountID(
+                    "sender_account_id".to_owned(),
+                    AccountID::parse_str("test1").unwrap_err()
+                )
+            ),
+            format!(
+                "Command: transfer {} test2 50\nStatus: error\nType: parse\nError: {}\n\n",
+                account1_id.to_string(),
+                ParseError::InvalidArgumentAccountID(
+                    "reciever_account_id".to_owned(),
+                    AccountID::parse_str("test2").unwrap_err()
+                )
+            ),
+            format!(
+                "Command: transfer test1 {} 50\nStatus: error\nType: parse\nError: {}\n\n",
+                account2_id.to_string(),
+                ParseError::InvalidArgumentAccountID(
+                    "sender_account_id".to_owned(),
+                    AccountID::parse_str("test1").unwrap_err()
+                )
+            ),
+            format!(
+                "Command: transfer {} {} test\nStatus: error\nType: parse\nError: invalid argument amount: {}\n\n",
+                account1_id.to_string(),
+                account2_id.to_string(),
+                "test".parse::<u64>().unwrap_err(),
+            ),
+            format!("Bank: 1\nOpID: {}\nStatus: ok\n\n", operations[2].id),
+            "Bank: 1\nStatus: error\nType: bank\nError: Insufficient funds\n\n".to_owned(),
+        ]
+        .join("");
+
+        assert_eq!(from_utf8(writer.as_slice()).unwrap(), expected);
+    }
 }
